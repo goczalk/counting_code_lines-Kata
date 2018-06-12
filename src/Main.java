ddc4fef38537db76abdb300299aa9a93fb7fa2a1
Klaudia Goczal <klaudia@lodz.pl>
2018-06-12 13:06:12 +0200

    Added ifs method with regex and inBlockComment

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,16 @@ public class Main {
     public static void main(String arg[]){
         int linesCountTrimSplitRegex = 0;
         int linesCountStream = 0;
+        int linesCountIfs = 0;
 
-        linesCountTrimSplitRegex = bufferedReaderMethod();
+        linesCountTrimSplitRegex = bufferedReaderAndTrimSplitMethod();
         System.out.println("Trim and split method: " + linesCountTrimSplitRegex);
 
         linesCountStream = streamMethod();
         System.out.println("Stream method: " + linesCountStream);
+
+        linesCountIfs = bufferedReaderAndIfsMethod();
+        System.out.println("Ifs method: " + linesCountIfs);
     }
 
     private static int streamMethod() {
@@ -40,13 +44,13 @@ public class Main {
         }
     }
 
-    private static int bufferedReaderMethod() {
-        int linesCountTrimSplitRegex = 0;
+    private static int bufferedReaderAndTrimSplitMethod() {
+        int lineCount = 0;
 
         try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
             String line;
             while ((line = br.readLine()) != null) {
-                linesCountTrimSplitRegex = trimSplitRegexMethod(linesCountTrimSplitRegex, line);
+                lineCount = trimSplitRegexMethod(line);
             }
 
         } catch (FileNotFoundException e) {
@@ -54,12 +58,55 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return linesCountTrimSplitRegex;
+        return lineCount;
     }
 
-    private static int trimSplitRegexMethod(int linesCount, String line) {
+    private static int bufferedReaderAndIfsMethod() {
+        int lineCount = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+
+            boolean inBlockComment = false;
+            while ((line = br.readLine()) != null) {
+
+                if(inBlockComment){
+                    if(line.matches("\\s*.*\\*\\/")) {
+                        inBlockComment = false;
+                    }
+                    continue;
+                }
+
+                else if(line.matches("\\s*[\\/]{2}.*")){
+                    continue;
+                }
+
+                else if(line.matches("\\s*")){
+                    continue;
+                }
+
+                else if(line.matches("\\s*\\/\\*.*")){
+                    if(line.matches("\\s*.*\\*\\/")){
+                        continue;
+                    }
+                    inBlockComment = true;
+                    continue;
+                }
+                lineCount++;
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lineCount;
+    }
+
+    private static int trimSplitRegexMethod(String line) {
         line = line.trim();
 
+        int linesCount = 0;
         if(!line.startsWith("//") && !line.startsWith("/*") && !line.startsWith("*")){
 
             String[] lineAfterSplit = line.split("\\s");
